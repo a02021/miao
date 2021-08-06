55f2fed6f8173dd4984f27e8411d72752d65cc1d
a02021 <uhur677guh@outlook.com>
2021-08-06 18:54:31 +0800

    lodash-reverse2

--- lodash/a02021-lodash.js
+++ lodash/a02021-lodash.js
@@ -572,11 +572,11 @@ function dropRightWhile(array,f) {
   function reverse(arr) {
     function swap(arr, a,b) {
       let c = arr[a]
-      a = b 
-      b = c
+      arr[a] = arr[b] 
+      arr[b] = c
     }
-    for (let i = 0; i < arr.length/2; i++) {
-      swap(arr,i,arr.length - i)
+    for (let i = 0; i < arr.length/2 ; i++) {
+      swap(arr,i,arr.length - i - 1)
     }
     return arr
   }

69f17ba7b93f7126612944d00f9852bc3ce020cb
a02021 <uhur677guh@outlook.com>
2021-09-01 16:15:23 +0800

    partition

--- lodash/a02021-lodash.js
+++ lodash/a02021-lodash.js
@@ -1562,6 +1562,37 @@ function dropRightWhile(array,f) {
     return false
   }
 
+  // Creates an array of elements split into two groups, the first of which contains elements predicate returns truthy for, the second of which contains elements predicate returns falsey for. The predicate is invoked with one argument: (value).
+  function partition(arr,pre) {
+    let p = f(pre)
+    let k = Object.keys(arr).reverse()
+    let r = []
+    let r2 = []
+    for (let i of k) {
+      if(p(arr[i])) r.push(arr[i])
+      if(!p(arr[i])) r2.push(arr[i])
+    }
+  
+    return [r,r2]
+      function f(p){
+      if(typeof p == 'function') return p
+      if(typeof p == 'string') return n => n[p]
+      if(Object.prototype.toString.call(p) == '[object Array]') {
+        return n => n[p[0]] == p[1]
+      }
+      if(Object.prototype.toString.call(p) == '[object Object]') {
+        return n => {
+          let kp = Object.keys(p)
+          let bol = true
+          for(let key of kp) {
+            if(n[key] !== p[key]) bol = false
+          }
+          if(bol) return true
+        }
+      }
+    }
+  }
+
   return {
     chunk:chunk,
     compact:compact,
@@ -1658,6 +1689,7 @@ function dropRightWhile(array,f) {
     groupBy:groupBy,
     size:size,
     includes:includes,
+    partition:partition,
   }
 } ();
 
